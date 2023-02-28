bf83121b3ebfa2dbff6d34cf85706cf2a87dba32
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2023-02-28 19:18:49 +0100

    xD

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello DevOps engineer from AWS!</p>
+        <p>Yekaterina Tsoy I Love You ♡ </p>
         <a
           className="App-link"
           href="https://reactjs.org"
